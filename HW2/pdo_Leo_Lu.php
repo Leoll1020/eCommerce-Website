5a36acecf20f60537a2f13058ac3f16c89a50b3f
Leoll1020 <clu10@uci.edu>
2016-04-27 11:53:25 -0700

    HW2 folder

---
+++ HW2/pdo_Leo_Lu.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "sylvester-mccoy-v3";
+$username = "inf124grp33";
+$password = "pux=C2ur";
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=inf124grp33", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+?>
