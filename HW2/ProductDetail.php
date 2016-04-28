9af49ccdf80abb5a59eda76ce3348530ea2d6086
godqhrdl <misooki89@gmail.com>
2016-04-28 16:33:05 -0700

    Update ProductDetail.php

--- HW2/ProductDetail.php
+++ HW2/ProductDetail.php
@@ -22,32 +22,6 @@ catch(PDOException $e)
 
 	?>
 
-<?php
-
-$stmt = $conn->query("SELECT * FROM products where product_id = $pid");
-
-
-echo '<table border="1">'."\n";
-while ( $row = $stmt->fetch(PDO::FETCH_ASSOC) ) {
-    echo("<tr><td>");
-    echo($row['product_id']);
-    echo("</td><td>");
-    echo($row['price']);
-    echo("</td><td>");
-    echo($row['product_name']);
-    echo("</td><td>");
-    echo($row['description']);
-    echo("</td><td>");
-    echo($row['features']);
-    echo("</td><td>");
-   echo '<img src="data:image/jpeg;base64,'.base64_encode( $row['image'] ).'"/>';
-    echo("</td><td>");
-     echo("</td></tr>");
-
-
-}
-echo "</table>\n";?>
-?>
 
 
 <!DOCTYPE html>
@@ -58,7 +32,8 @@ echo "</table>\n";?>
 		<meta name = "Description", content = "Product Details">
 		<meta name = "keywords", content = "IMAGE, PRICE, INFO">
 		<meta name = "author", content = "Jessica Zeng Chen Lu">
-		<link rel="stylesheet" href="css/product_detail.css">		
+	
+		<link rel="stylesheet" href="css/product_detail.css" type="text/css">	
 	</head>
 <body class = "body">
 	<hearder id=header>
@@ -94,39 +69,60 @@ echo "</table>\n";?>
 		</ul>
 	</div>
 
+<?php
 
-	<div id = "row3">
-		<div id = "info">
+$stmt = $conn->query("SELECT * FROM products where product_id = $pid");
+
+
+?>
+
+<?php
+	while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+?>
+
 			<table id = "product_detail">
-				<tr><h1 id = "title">HP Black Licorice 15.6" 15-F387WM Laptop PC</h1></tr>
-				<tr><h3 id = "price">Price: $320</h3></tr>
+				<tr><h1 id = "title"> <?php echo $row['product_name'] ?> </h1></tr>
+				<tr><h3 id = "price"><?php echo "Price $" . $row['price'] ?></h3></tr>
 				<tr><h3 id = "ship">Free Shipping</h3></tr>
 				<tr><a href = "form.html"><img id ="buy" src = "img/buy.jpg" alt = "buy now" style = "width:200px;height:80px;"></a></tr>
-				<tr><p id = "details">Get the perfect combination of style and productivity, while keeping your wallet in mind — now that's something to get excited about.
-				</p></tr>
+				<tr><p id = "details"><?php echo $row['description'] ?></p>
+				</tr>
 				<tr><h4 id = "feature_title">Product Features</h4>
-				<div id ="feature">
-					<p >style ="background-color:#C8CBCC">AMD A8-7410 processor</p>
-					<p>2.20GHz (with Max Turbo Speed of 2.5GHz)</p>
-					<p style ="background-color:#C8CBCC">4GB DDR3 SDRAM system memory</p>
-					<p>500GB SATA hard drive</p>
-					<p style ="background-color:#C8CBCC">SuperMulti DVD burner</p>
-					<p>10/100Base-T Fast Ethernet, 802.11b/g/n Wireless LAN</p>
-					<p style ="background-color:#C8CBCC">Rechargeable lithium-ion battery</p>
-					<p>15.6" HD WLED-backlit touchscreen/p>
-					<p>style ="background-color:#C8CBCC">BGenuine Microsoft Windows 10 Home</p>
-					<p>Media Formats: popular media formats</p>
-					<p style ="background-color:#C8CBCC">3-cell lithium-ion polymer battery</p>
+					<div id ="feature">
+					<?php
+						$feature = explode(",",$row['features']);
+                                                $a=true;
+						while  (list(,$value) = each($feature)){
+					?>
+                                            <?php 
+                                            if ($a === true){
+                                                
+                                            ?>
+                                                <p style ="background-color:#C8CBCC"><?php echo $value ?></p>
+                                            <?php $a = false;
+                                            ?>
+                                             <?php
+                                                } else  { ?>
+                                                        <p><?php echo $value ?></p>
+                                        <?php
+                                                 $a = true;
+                                            ?>
+                                               
+                                                <?php } ?>
+					
+				<?php } ?>	
 				</div>
 				</tr>
 			</table>
-			</div>
+		</div>		
 		<div id = "picture">
-			<div id = "item1"><img  src= "img/hpblack1.jpg" alt = "hpblack" ></div>
-			<div id = "item2"><img  src = "img/hpblack2.jpg" alt = "hpblack"></div>
-			<div id = "item3"><img  src = "img/hpblack3.jpg" alt = "hpblack"></div>	
+			<div id = "item1"><img  src= <?php echo $row['image'] ?> alt = "accer" ></div>
+			<div id = "item2"><img  src = <?php echo $row['image'] ?>alt = "accer"></div>
+			<div id = "item3"><img  src = <?php echo $row['image'] ?> alt = "accer"></div>	
 		</div>
 	</div>
+
+<?php } ?>
 
 	
 
