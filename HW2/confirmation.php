dc2e3623fa8913ba40e9f3fea5bfce36dfc4f391
songyj1220 <songyj1220@gmail.com>
2016-05-01 20:53:34 -0700

    Added files via upload

---
+++ HW2/confirmation.php
@@ -0,0 +1,63 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Contact</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="css/contact.css" type="text/css">
+  <meta name="author" content="Misook Sohn">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+
+</head>
+
+<body class="body">
+<hearder id=header>
+  <h1>BEST Electronics</h1>
+</hearder>
+  <nav><ul>
+    <li><a href="Homepage.html">Home</a></li>
+        <a href="Homepage1.html"> </a>
+    <li><a href="About.html">About</a></li>
+    <li class="dropdown">
+    <a href="#" class="dropbtn">Products</a>
+    <div class="dropdown-content">
+      <a href="cellphone.html">Cellphones</a>
+      <a href="laptop.html">Laptops</a>
+      <a href="tablet.html">Tablets</a>
+    </div>
+    </li>
+    <li><a href="Contact.html">Contact</a></li>
+  </nav></ul>   
+
+  </div>
+  <section id ="section"> 
+
+  <header>
+    
+    <h2>Order Confirmation</h2>
+    <h2>Your order is processed successfully.</h2>
+    <h2>Thank you for placing your order with BEST Electronics!</h2>
+  </header>
+<div class="img">
+
+  		<a target="_blank" href="img_fjords.jpg">
+
+    			<img src="http://www.maconcfb.org/sites/default/files/news/Contentulations.jpg" width="600" height="400">
+
+  		</a>
+	
+	</div>
+
+             
+   
+    
+    
+  </div>
+</section>
+ 
+
+  <div class="footer">
+    <p>BEST Electronics</p>
+  </footer>
+</body>
+</html>
